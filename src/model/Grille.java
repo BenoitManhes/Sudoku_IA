b66252a2f9e9929c1a0784a34bf2926f30e72eda
Benoit Manhes <benoit.manhes.82@gmail.com>
2018-10-05 19:10:12 -0400

    structure model

---
+++ src/model/Grille.java
@@ -0,0 +1,69 @@
+package model;
+
+public class Grille {
+
+	private int[][] grille;
+
+	public Grille() {
+		this.grille = new int[9][9];
+		// initialisation("nomFichier.txt"); 
+	}
+
+	/** ================================================ methode de calcul =====================================================================*/
+	public void init(String fichier) {
+		// initialisation avec un fichier text
+	}
+
+	// check valeur selon le carre ou elle est placee
+	public boolean checkCarre(int x, int y, int value) {
+		boolean check = true;
+		if(this.checkCaseDispo(x, y)) {
+			int X = x/3;
+			int Y = y/3;
+
+			for (int i = Y; i < Y+3; i++) {
+				for (int j = X; j < X+3; j++) {
+					if(this.grille[i][j] == value) check = false;
+				}
+			}
+		}
+
+		return check;
+	}
+
+	// check valeur selon la ligne et la colonne
+	public boolean checkLineaire(int x, int y, int value) {
+		boolean check = true;
+		if(this.checkCaseDispo(x, y)) {
+			//check ligne & colonne
+			for (int i = 0; i < grille.length; i++) {
+				//check ligne
+				if(this.grille[y][i] == value) check = false;
+				//check colonne
+				if(this.grille[i][x] == value) check = false;
+			}
+		}
+
+		return check;
+	}
+
+	// check que la case soit dans la grille et vide
+	public boolean checkCaseDispo(int x, int y) {
+		boolean check = false;
+		// check si coordonne dans la grille
+		if( x>=0 && y>=0 && y<this.grille.length && x<this.grille.length) {
+			//check si case vide
+			if(this.grille[y][x]==0) {
+				check = true;
+			}
+		}
+
+		return check;
+	}
+
+
+	/** ================================================= getter et setter =====================================================================*/
+	public int[][] getGrille() {
+		return this.grille;
+	}
+}
