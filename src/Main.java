dc889e4e23ffebb46982f52bff99947a9422dad0
MrPierroo <ps.15895@hotmail.fr>
2018-10-10 19:30:38 -0400

    Utilisation d'url

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import model.Backtracking;
 import model.Sudoku;
 import view.ViewSudoku;
 
 public class Main {
 
-	public static void main(String[] args) {
-		File fichier = new File("exemple1.txt");
+	public static void main(String[] args) throws IOException {
+		URL url = Main.class.getResource("/resources/exemple1.txt");
+		File fichier = new File(url.toString().substring(5));
 		Sudoku sudoku = new Sudoku(fichier);
 		ViewSudoku viewSudoku = new ViewSudoku();
 		sudoku.addObserver(viewSudoku);
-		
+
 		sudoku.actualize();
 		//Backtracking.solve(sudoku);
 
