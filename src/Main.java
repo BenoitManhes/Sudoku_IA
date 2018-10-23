cf212bfbebded794010ab8f6052ff9a268253105
MrPierroo <ps.15895@hotmail.fr>
2018-10-23 15:05:19 -0400

    Mrv <-> Dh

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ import view.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		//URL url = Main.class.getResource("C:\\Users\\Benoît\\workspace\\Sudoku_IA\\src\\resources\\exemple1.txt");
-		File fichier = new File("C:\\Users\\Benoît\\workspace\\Sudoku_IA\\src\\resources\\exemple1.txt");
+		URL url = Main.class.getResource("/resources/exemple1.txt");
+		File fichier = new File(url.toString().substring(5));
 		Sudoku sudoku = new Sudoku(fichier);
 		ViewSudoku viewSudoku = new ViewSudoku();
 		sudoku.addObserver(viewSudoku);
--- src/model/CaseComparator.java
+++ src/model/CaseComparator.java
@@ -6,15 +6,15 @@ public class CaseComparator implements Comparator<Case>{
 
 	@Override
 	public int compare(Case A, Case B) {
-		if(A.getPriorityDh() == B.getPriorityDh()){
-			if(A.getPriorityMrv() >= B.getPriorityMrv()){
+		if(A.getPriorityMrv() == B.getPriorityMrv()){
+			if(A.getPriorityDh() >= B.getPriorityDh()){
 				return 1;
 			}
 			else{
 				return -1;
 			}
 		}
-		else if(A.getPriorityDh() > B.getPriorityDh()){
+		else if(A.getPriorityMrv() > B.getPriorityMrv()){
 			return 1;
 		}
 		else{
